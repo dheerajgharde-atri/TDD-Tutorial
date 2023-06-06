8a91f7ab7b9d14ea6d98b2cc060ef6f12dc3e26c
dheerajgharde-atri <gharde@atrihub.io>
2023-06-06 14:58:02 -0700

    test(functional_test): TDD Chapter 2
    Used the unittest module to build a class and implement one functional test and describe the rest
    
    [ICTRAINING-1509]

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,38 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_start_list_and_retreive_later(self):
+        # To checkout homepage
+        self.browser.get('http://localhost:8000')
+
+        # To-do in the header of the web page
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+        # Entering a to-do item
+
+        # Enter "buy-peacock-feathers" in text box
+
+        # On hitting enter, the page updates, and the page lists "1: Buy peacock feathers" as an item in a to-do list
+
+        # Use textbox to add another to-do "Use peacock feathers to make a fly"
+
+        # Page updates and shows both the items on the list
+
+        # Unique url is generated for the user and there is an explanatory text to that effect
+
+        # Visiting the unique url just to see if its still there
+
+        # Goes back to sleep
+
+
+if __name__ == '__main__':
+    unittest.main()
