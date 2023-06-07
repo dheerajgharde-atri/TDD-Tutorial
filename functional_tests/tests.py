4489741e114b7b0844e7858682940863c50e1d9b
dheerajgharde-atri <gharde@atrihub.io>
2023-06-07 16:31:02 -0700

    test(functional test): TDD Chapter 6
    Replaced time.sleep with a custom function to avoid hardcoding
    
    [ICTRAINING-1513]

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,21 +1,32 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
 import time
-import unittest
 from django.test import LiveServerTestCase
+
+MAX_WAIT = 10
 
 
 class NewVisitorTest(LiveServerTestCase):
+
     def setUp(self):
         self.browser = webdriver.Firefox()
 
     def tearDown(self):
         self.browser.quit()
 
-    def check_row_in_table(self, row_text):
-        table = self.browser.find_element("id", "id_list_table")
-        rows = table.find_elements("tag name", "tr")
-        self.assertIn(row_text, [row.text for row in rows])
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element('id', 'id_list_table')
+                rows = table.find_elements('tag name', 'tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
 
     def test_start_list_and_retreive_later(self):
         # To checkout homepage
@@ -35,17 +46,14 @@ class NewVisitorTest(LiveServerTestCase):
 
         # On hitting enter, the page updates, and the page lists "1: Buy peacock feathers" as an item in a to-do list
         input_box.send_keys(Keys.ENTER)
-        time.sleep(1)
-
-        self.check_row_in_table('1: Buy peacock feathers')
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
         # Use textbox to add another to-do "Use peacock feathers to make a fly"
         input_box = self.browser.find_element("id", 'id_new_item')
         input_box.send_keys('Use peacock feathers to make a fly')
         input_box.send_keys(Keys.ENTER)
-        time.sleep(2)
 
-        self.check_row_in_table('2: Use peacock feathers to make a fly')
+        self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
         # Page updates and shows both the items on the list
 
