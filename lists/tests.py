43a4392881ddcf7a7af744b45cf0b8896ef2d3ad
dheerajgharde-atri <gharde@atrihub.io>
2023-06-07 14:46:18 -0700

    feat(lists app): TDD Chapter 5
    Added a new model called Item along with unit tests
    
    [ICTRAINING-1512]

--- lists/tests.py
+++ lists/tests.py
@@ -3,6 +3,7 @@ from django.urls import resolve
 from lists.views import home_page
 from django.test import TestCase
 from django.template.loader import render_to_string
+from lists.models import Item
 
 
 # Create your tests here.
@@ -21,3 +22,21 @@ class HomePageTest(TestCase):
         response = self.client.post('/', data={'item_text': 'A new list item'})
         self.assertIn('A new list item', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
+
+    def test_database_saves_retrieves_items(self):
+        first_item = Item()
+        first_item.text = "First list item"
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = "Second list item"
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, "First list item")
+        self.assertEqual(second_saved_item.text, "Second list item")
+
