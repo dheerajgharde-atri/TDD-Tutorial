10f4903f1048906a64d068c06df6f97c97b47cf3
dheerajgharde-atri <gharde@atrihub.io>
2023-06-08 14:28:51 -0700

    test(functional tests): TDD Chapter 7
    Added functional test to check for unique url for each user
    
    [ICTRAINING-1519]

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -63,4 +63,41 @@ class NewVisitorTest(LiveServerTestCase):
 
         # Goes back to sleep
 
-        self.fail('Finish the test!')
+
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        # Edith starts a new to-do list
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element('id', 'id_new_item')
+        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
+
+        # She notices that her list has a unique URL
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
+
+        # Now, new user comes along
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        # visiting home page
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element('tag name', 'body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+
+        # starts a list
+        inputbox = self.browser.find_element('id', 'id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy milk')
+
+        # new url
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        # No trace of other user's list
+        page_text = self.browser.find_element('tag name', 'body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
